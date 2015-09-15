073ae588411f99868152101444fa94a170179c22
Michael F <mikey@mikeys-MacBook-Pro.local>
2015-09-14 18:13:41 -0700

    lesson_12

--- 12_Rails_Intro_Controller_Continued/code_along/bookazon_app/app/controllers/books_controller.rb
+++ 12_Rails_Intro_Controller_Continued/code_along/bookazon_app/app/controllers/books_controller.rb
@@ -6,25 +6,50 @@ class BooksController < ApplicationController
   end
 
   def show
+    @book = find_book
   end
 
   def new
+    @book = Book.new
   end
 
   def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book)
+    else
+      render 'new'
+    end
   end
 
   def edit
+    @book = find_book
   end
 
   def update
+    @book = find_book
+    if @book.update_attributes(book_params)
+      redirect_to book_path(@book)
+    else
+      render 'new'
+    end
   end
 
   def destroy
+    @book = find_book
+    @book.destroy
+    redirect_to books_path
   end
 
-  private
+# delete link for view
+# <%= link_to 'Delete', book_path(@book), :method => :delete, data: { confirm: 'Are you sure?' } %>
 
+  private
   def book_params
+    params.require(:book).permit(:title, :author)
+  end
+
+  def find_book
+    Book.find(params[:id])
   end
 end
